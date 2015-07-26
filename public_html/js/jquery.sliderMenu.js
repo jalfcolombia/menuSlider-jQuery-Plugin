809383a79792ba93963b0fdf1bd29ec41e32c3a4
Julian Lasso <julian.lasso@gmail.com>
2015-07-26 12:56:23 -0500

    Se corrige error que bloqueaba el scroll vertical cunado se cerraba el menuSlider

--- public_html/js/jquery.sliderMenu.js
+++ public_html/js/jquery.sliderMenu.js
@@ -4,9 +4,9 @@ jQuery.sliderMenu = function (menu) {
   if (typeof menu.close != 'undefined') {
     menu.close = '#' + menu.close;
   }
-  
+
   $(menu.menu).addClass('slider-menu slider-menu-' + menu.direction);
-  
+
   $(menu.button).click(function () {
     $(menu.menu).addClass('slider-menu-active slider-menu-active-' + menu.direction);
     $('body').addClass('slider-menu-active-body');
@@ -15,13 +15,23 @@ jQuery.sliderMenu = function (menu) {
     $('#screenBlack').click(function () {
       $.sliderMenuOff(menu.menu, menu.direction);
     });
-    $('body').swipe({
-      swipe: function (event, direction, distance, duration, fingerCount) {
-        if (direction == menu.direction) {
-          $.sliderMenuOff(menu.menu, menu.direction);
-        }
-      }
-    });
+
+    switch (menu.direction) {
+      case 'left':
+        options = {swipeLeft: function (event, direction, distance, duration, fingerCount) {
+            $.sliderMenuOff(menu.menu, menu.direction);
+          }
+        };
+        break;
+      case 'right':
+        options = {swipeRight: function (event, direction, distance, duration, fingerCount) {
+            $.sliderMenuOff(menu.menu, menu.direction);
+          }
+        };
+        break;
+    }
+
+    $('body').swipe(options);
 
     if (typeof menu.close != 'undefined') {
       $(menu.close).click(function () {
